dc8454e6aee7017cd46214379231e9d1e08b83b7
Ahmad <ssahmadtijani@gmail.com>
2018-01-18 12:43:12 +0100

    run on local system

--- platforms/browser/www/assets/js/index.js
+++ platforms/browser/www/assets/js/index.js
@@ -47,3 +47,100 @@
         console.log('Received Event: ' + id);
     }
 };*/
+
+    document.getElementById("createFile").addEventListener("click", createFile);
+    document.getElementById("writeFile").addEventListener("click", writeFile);
+    document.getElementById("readFile").addEventListener("click", readFile);
+    document.getElementById("removeFile").addEventListener("click", removeFile);
+
+function createFile() {
+    var type = window.PERMANENT;
+    var size = 5*1024*1024;
+    window.requestFileSystem(type, size, successCallback, errorCallback)
+ 
+    function successCallback(fs) {
+       fs.root.getFile('log.txt', {create: true, exclusive: true}, function(fileEntry) {
+          alert('File creation successfull!')
+       }, errorCallback);
+    }
+ 
+    function errorCallback(error) {
+       alert("ERROR: " + error.code)
+    }
+     
+ }
+
+
+ function writeFile() {
+    var type = window.PERMANENT;
+    var size = 5*1024*1024;
+    window.requestFileSystem(type, size, successCallback, errorCallback)
+ 
+    function successCallback(fs) {
+       fs.root.getFile('log.txt', {create: true}, function(fileEntry) {
+ 
+          fileEntry.createWriter(function(fileWriter) {
+             fileWriter.onwriteend = function(e) {
+                alert('Write completed.');
+             };
+ 
+             fileWriter.onerror = function(e) {
+                alert('Write failed: ' + e.toString());
+             };
+ 
+             var blob = new Blob(['Lorem IpsumOIOIOII'], {type: 'text/plain'});
+             fileWriter.write(blob);
+          }, errorCallback);
+       }, errorCallback);
+    }
+ 
+    function errorCallback(error) {
+       alert("ERROR: " + error.code)
+    }
+ }
+
+
+ function readFile() {
+    var type = window.PERMANENT;
+    var size = 5*1024*1024;
+    window.requestFileSystem(type, size, successCallback, errorCallback)
+ 
+    function successCallback(fs) {
+       fs.root.getFile('log.txt', {}, function(fileEntry) {
+ 
+          fileEntry.file(function(file) {
+             var reader = new FileReader();
+ 
+             reader.onloadend = function(e) {
+                var txtArea = document.getElementById('textarea');
+                txtArea.value = this.result;
+             };
+             reader.readAsText(file);
+          }, errorCallback);
+       }, errorCallback);
+    }
+ 
+    function errorCallback(error) {
+       alert("ERROR: " + error.code)
+    }
+ }	
+
+
+ function removeFile() {
+    var type = window.PERMANENT;
+    var size = 5*1024*1024;
+    window.requestFileSystem(type, size, successCallback, errorCallback)
+ 
+    function successCallback(fs) {
+       fs.root.getFile('log.txt', {create: false}, function(fileEntry) {
+ 
+          fileEntry.remove(function() {
+             alert('File removed.');
+          }, errorCallback);
+       }, errorCallback);
+    }
+ 
+    function errorCallback(error) {
+       alert("ERROR: " + error.code)
+    }
+ }	
